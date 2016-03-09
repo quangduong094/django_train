d3b3c5b321eca5187937ba28fa4672bc8782f8c6
quangduong094 <quangduong0942@gmail.com>
2016-03-09 15:01:35 +0700

    add __str__() to models

--- polls/models.py
+++ polls/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.utils.encoding import python_3_unicode_compatible
-
+from django.utils import timezone
 # Create your models here.
 @python_3_unicode_compatible
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
-	def __str__(self):
-		return self.question_text
+	def question_str(self):
+        return self.question_text
+
+
+        
 
 @python_3_unicode_compatible
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
-	def __str__(self):
+	def choice_str(self):
 		return self.choice_text
 		
