dd6c7a3023a442b73601a37915846df48b443040
quangduong094 <quangduong0942@gmail.com>
2016-03-11 15:54:55 +0700

    shortcut render  404 error

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.http import Http404
+#from django.http import Http404
 #from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render
 from .models import Question
 
@@ -16,10 +17,11 @@ def index(request):
 
 def detail(request, question_id):
     #return HttpResponse("You're looking at question %s." %question_id)
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
