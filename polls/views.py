8790ec1a8ae3dfaed8ffe9c66f7691af10d06b36
quangduong094 <quangduong0942@gmail.com>
2016-03-11 17:34:20 +0700

    use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,36 +1,26 @@
-from django.http import HttpResponseRedirect, HttpResponse
-#from django.http import Http404
-#from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-# from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    #return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    #return HttpResponse("You're looking at question %s." %question_id)
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    # response = "You're looking at the results of question %s." 
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+        
+        
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." %question_id)
     question = get_object_or_404(Question, pk= question_id)
